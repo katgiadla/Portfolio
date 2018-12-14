a06e45f88401b3990c10560eb78c597163c61dbe
Tchorson <tchorek.mateusz@gmail.com>
2018-12-14 08:56:45 +0100

    Final FEM program. Not refractored yet.

--- Java - College/MES2018/src/mes/funkcje/TemperatureIteration.java
+++ Java - College/MES2018/src/mes/funkcje/TemperatureIteration.java
@@ -1,5 +1,7 @@
 package mes.funkcje;
 
+import Jama.Matrix;
+
 public class TemperatureIteration {
     private double[][] globalMatrixHnoBorders;
 
@@ -20,7 +22,7 @@ public class TemperatureIteration {
     private double simulationTime;
     private double stepTime;
     private double initialTemperature;
-
+    //private Macierz macierz;
 
     public TemperatureIteration(double[][] globalMatrixH, double[][] globalMatrixHnoBorders, double[][] globalMatrixC, double[] load_vector ,GlobalData globalData) {
         this.globalMatrixH = globalMatrixH;
@@ -45,18 +47,8 @@ public class TemperatureIteration {
         //this.print();
         H_Prime = this.add_H_toCdividedBydeltaTau(globalMatrixH,globalMatrixC,stepTime);
 
-        //this.doFem();
-
-
-         double[][] test = {{1,3,5,7},{6,9,4,7},{3,2,7,4},{1,5,5,7}};
-        double [][] transponowana = wyznaczMacierzOdwrotna(test);
-        double [][] odwrotna = pomnozPrzezSkalarTablice(1/wyznaczWyznacznikMacierzy(test), transponowana);
-        for(int i=0;i<test.length;i++){
-            for(int j=0;j<test[0].length;j++){
-                System.out.print(odwrotna[i][j]+" ");
-            }
-            System.out.println();
-        }
+        this.doFem();
+        //this.add_P_toCdividedByDeltaTauMultipliedByInitialTempVector(stepTime,globalMatrixC,temperature_0,load_vector);
     }
 
     private void doFem(){
@@ -68,65 +60,29 @@ public class TemperatureIteration {
         P_Prime = this.add_P_toCdividedByDeltaTauMultipliedByInitialTempVector(stepTime,globalMatrixC,tmpTemp,load_vector);
 
         for(int i=0;i<(simulationTime/stepTime);i++){
-            nextTemperature = this.calculateFemEquation(H_Prime,tmpTemp,P_Prime);
+            nextTemperature = this.calculateFemEquation(H_Prime,P_Prime);
             tmpTemp = nextTemperature;
-            P_Prime = this.add_P_toCdividedByDeltaTauMultipliedByInitialTempVector(stepTime,globalMatrixC,tmpTemp,P_Prime);
+            P_Prime = this.add_P_toCdividedByDeltaTauMultipliedByInitialTempVector(stepTime,globalMatrixC,tmpTemp,load_vector);
             this.findMinMaxTemp(tmpTemp,(i+1)*stepTime);
         }
     }
- /* http://www.darmoweskrypty.linuxpl.info/macierz/java - stad zaczerpniete polskie wzory*/
-
-    public double[][] wyznaczMacierzOdwrotna(double [][]inputArray) {
-        if (inputArray[0].length != inputArray.length) {
-            throw new RuntimeException("array is not N x N Dimension");
-        }
-        double[][] reversedArray = new double[inputArray.length][inputArray[0].length];
-
-        double[][] macierzDolaczona = new double[inputArray.length][inputArray[0].length];
-        for (int i = 0; i < inputArray.length; i++) {
-            for (int j = 0; j < inputArray[0].length; j++) {
-                double[][] temp = new double[inputArray.length - 1][inputArray[0].length - 1];
-                int a = 0, b = 0;
-                for (int w = 0; w < inputArray.length; w++) {
-                    for (int z = 0; z < inputArray[0].length; z++) {
-                        if (w != i && z != j) {
-                            if (b >= temp.length) {
-                                b = 0;
-                                a++;
-                            }
-                            temp[a][b] = inputArray[w][z];
-                            b++;
-                        }
-                    }
-                }
-
-                double wyznacznikTemp = this.wyznaczWyznacznikMacierzy(temp);
-
-                if ((i + j) % 2 != 0) {//Niparzyste czyli zmiana znaku wyznacznika
-                    if (wyznacznikTemp > 0) {
-                        wyznacznikTemp -= 2 * wyznacznikTemp;
-                    } else {
-                        wyznacznikTemp -= 2 * wyznacznikTemp;
-                    }
-                } else {
-                }
-                macierzDolaczona[i][j] = wyznacznikTemp;
-            }
-        }
-        macierzDolaczona = this.transponujTablice(macierzDolaczona);
-        //reversedArray = this.pomnozPrzezSkalarTablice(1 / this.wyznaczWyznacznikMacierzy(this.tablica), macierzDolaczona);
-        return (macierzDolaczona);
 
 
-    }
-
-
-
-    private double[] calculateFemEquation(double[][]matrixH, double[] startTemperatureVector, double[] globalLoadVector){
+    private double[] calculateFemEquation(double[][]matrix_H_Prime, double[] globalLoadVector){
         double [] temperatureVectorResult = new double[load_vector.length];
 
-        double [][] macierzDolaczona = this.wyznaczMacierzOdwrotna(matrixH);
-        double [][] reversed_H_prime = pomnozPrzezSkalarTablice(1/this.wyznaczWyznacznikMacierzy(matrixH),macierzDolaczona);
+
+
+        Matrix matrix = new Matrix(matrix_H_Prime);
+        Matrix inverseMatrix = matrix.inverse();
+        double[][] inverseMatrixH = inverseMatrix.getArray();
+
+        for(int i = 0;i<inverseMatrixH.length;i++){
+            for(int j =0;j<inverseMatrixH[0].length;j++){
+                temperatureVectorResult[i]+=(inverseMatrixH[i][j]*globalLoadVector[j]);
+            }
+            //System.out.println(temperatureVectorResult[i]);
+        }
 
 
         return temperatureVectorResult;
@@ -139,6 +95,7 @@ public class TemperatureIteration {
             for(int j = 0;j<H_Iteration[0].length;j++){
                 H_Iteration[i][j]+=matrixH[i][j]+ matrixC[i][j]/inputStepTime;
             }
+
         }
         //this.print(H_Iteration);
         return H_Iteration;
@@ -148,24 +105,17 @@ public class TemperatureIteration {
         double [] P_Iteration = new double[load_vector.length];
         for(int i =0;i<load_vector.length;i++) {
             for (int j = 0; j < load_vector.length; j++) {
-                P_Iteration[i] += -(matrixC[i][j] / inputStepTime) * temperature_0[j];
+                P_Iteration[i] += (matrixC[i][j] / inputStepTime) * temperature_0[j];
             }
             P_Iteration[i]+=inputLoadVector[i] ;
-            P_Iteration[i]= -P_Iteration[i];
+            //System.out.println(P_Iteration[i]);
+            //P_Iteration[i]= -P_Iteration[i];
             //System.out.print(P_Iteration[i]+" ");
         }
+        System.out.println();
         return P_Iteration;
     }
 
-    /*private double [] multiplyMatrixCdividedByDeltaTauByInitialTempVector(){
-        double [] result = new double[load_vector.length];
-                for(int i=0;i<globalMatrixC.length;i++){
-                    for(int j =0;j<globalMatrixC[0].length;j++){
-                        result[i] += ;
-                    }
-                }
-                return result;
-    }*/
 
     private void findMinMaxTemp(double [] temp, double step){
         double min = temp[0], max = temp[0];
@@ -190,109 +140,6 @@ public class TemperatureIteration {
             input[i]=0;
         }
         return input;
-    }
-
-
-    /*public double matrixDeterminant (double[][] matrix) {
-        double temporary[][];
-        double result = 0;
-
-        if (matrix.length == 1) {
-            result = matrix[0][0];
-            return (result);
-        }
-
-        if (matrix.length == 2) {
-            result = ((matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][0]));
-            return (result);
-        }
-
-        for (int i = 0; i < matrix[0].length; i++) {
-            temporary = new double[matrix.length - 1][matrix[0].length - 1];
-
-            for (int j = 1; j < matrix.length; j++) {
-                for (int k = 0; k < matrix[0].length; k++) {
-                    if (k < i) {
-                        temporary[j - 1][k] = matrix[j][k];
-                    } else if (k > i) {
-                        temporary[j - 1][k - 1] = matrix[j][k];
-                    }
-                }
-            }
-
-            result += matrix[0][i] * Math.pow (-1, (double) i) * matrixDeterminant (temporary);
-        }
-        return (result);
-    }*/
-
-
-    private double[][] pomnozPrzezSkalarTablice(double skalar, double[][] tablica) {
-        double[][] macierzPomnozona = new double[tablica.length][tablica[0].length];
-        for (int i = 0; i < tablica.length; i++) {
-            for (int j = 0; j < tablica[0].length; j++) {
-                macierzPomnozona[i][j] = (tablica[i][j] * skalar);
-            }
-        }
-        return macierzPomnozona;
-    }
-
-    private double[][] transponujTablice(double[][] tablica) {
-        double[][] macierzTransponowana = new double[tablica[0].length][tablica.length];
-        for (int i = 0; i < tablica.length; i++) {
-            for (int j = 0; j < tablica[0].length; j++) {
-                macierzTransponowana[j][i] = tablica[i][j];
-            }
-        }
-        return macierzTransponowana;
-    }
-
-    private double wyznaczWyznacznikMacierzy(double[][] tablica) {
-        double wyznacznik = 0;
-
-        if (tablica.length == 1 && tablica[0].length == 1) {
-            wyznacznik = tablica[0][0];
-        } else if (tablica.length != tablica[0].length) {
-            throw new RuntimeException("Nie można wyznaczyć wyznacznika dla macierzy która nie jest kwadratowa");
-        } else if (tablica.length == 2 && tablica[0].length == 2) {
-            wyznacznik = (tablica[0][0] * tablica[1][1] - tablica[0][1] * tablica[1][0]);
-        } else {
-            double[][] nTab = new double[tablica.length + (tablica.length - 1)][tablica[0].length];
-            for (int i = 0, _i = 0; i < nTab.length; i++, _i++) {
-                for (int j = 0; j < tablica[0].length; j++) {
-                    if (_i < tablica.length && j < tablica[0].length) {
-                        nTab[i][j] = tablica[_i][j];
-                    } else {
-                        _i = 0;
-                        nTab[i][j] = tablica[_i][j];
-                    }
-                }
-            }
-
-            double iloczyn = 1;
-            int _i;
-
-            for (int i = 0; i < tablica.length; i++) {
-                _i = i;
-                for (int j = 0; j < tablica[0].length; j++) {
-                    iloczyn *= nTab[_i][j];
-                    _i++;
-                }
-                wyznacznik += iloczyn;
-                iloczyn = 1;
-            }
-
-            iloczyn = 1;
-            for (int i = 0; i < tablica.length; i++) {
-                _i = i;
-                for (int j = tablica[0].length - 1; j >= 0; j--) {
-                    iloczyn *= nTab[_i][j];
-                    _i++;
-                }
-                wyznacznik -= iloczyn;
-                iloczyn = 1;
-            }
-        }
-        return wyznacznik;
     }
 
 
