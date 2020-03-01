5ee8db73b50c5973d0458cc13965ab18c8c548a4
Tchorson <tchorek.mateusz@gmail.com>
2020-03-01 10:08:12 +0100

    Old code remove

--- Own projects/PDF_To_XLSX_Converter/src/main/java/tchorek/Main.java
+++
@@ -1,9 +0,0 @@
-package tchorek;
-
-public class Main {
-
-    public static void main(String[] args) {
-        PDFReader pdfReader = new PDFReader();
-        pdfReader.convertPDFToXLSX();
-    }
-}
